8432f40132162a7deae6977603fec15f017a5e9c
tsunekawa <tsunekawa.masafumi@bpsinc.jp>
2021-07-16 18:59:26 +0900

    getStaticPaths getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,54 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 
+// export async function getStaticPaths() {
+//   return {
+//     // params.idとして固定値1と2を渡す
+//     paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+//     fallback: false
+//   }
+// }
+
+// export async function getStaticProps({ params }) {
+//   // getStaticPathsから渡されるparamsでid1と2を固定で受け取る
+//   const res = await fetch(`https://.../posts/${ params.id }`);
+//   const posts = await res.json();
+//   // pave componentにpropsとして渡される
+//   return { props: { posts } }
+// }
+
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={ styles.container }>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className={ styles.main }>
+        <h1 className={ styles.title }>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p className={ styles.description }>
+          Get started by editing{ ' ' }
+          <code className={ styles.code }>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className={ styles.grid }>
+          <a href="https://nextjs.org/docs" className={ styles.card }>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" className={ styles.card }>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className={ styles.card }
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -40,7 +56,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className={ styles.card }
           >
             <h3>Deploy &rarr;</h3>
             <p>
@@ -50,14 +66,14 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={ styles.footer }>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by{ ' ' }
+          <img src="/vercel.svg" alt="Vercel Logo" className={ styles.logo }/>
         </a>
       </footer>
     </div>
